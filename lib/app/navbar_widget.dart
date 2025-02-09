eaf8338d01181c0d166f3306780a121b5591825f
AbdelDjaouad Azzouz <imjawd@gmail.Com>
2025-02-09 21:22:30 +0100

    Update navbar_widget.dart

--- lib/app/navbar_widget.dart
+++ lib/app/navbar_widget.dart
@@ -14,45 +14,67 @@ class BottomNavBar extends StatelessWidget {
       height: 60,
       width: MediaQuery.of(context).size.width,
       child: Container(
-        decoration: BoxDecoration(border: Border(top: BorderSide(color: Colors.grey.withValues(alpha: 0.05)))),
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Colors.grey.withValues(alpha: 0.05)),
+          ),
+        ),
         child: Acrylic(
           elevation: 30,
           child: Padding(
             padding: const EdgeInsets.all(1),
             child: StreamBuilder(
-                stream: routes.currentRouteIndex.stream,
-                builder: (context, snapshot) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ...routes.allRoutes.where((r) => r.navbarTitle.isNotEmpty).map((r) => BottomNavBarButton(
-                            icon: r.icon,
-                            identifier: r.identifier,
-                            title: r.navbarTitle,
-                            active: r.identifier == routes.currentRoute.identifier,
-                          )),
-                      const Divider(direction: Axis.vertical),
-                      FlyoutTarget(
+              stream: routes.currentRouteIndex.stream,
+              builder: (context, snapshot) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ...routes.allRoutes
+                        .where((r) => r.navbarTitle.isNotEmpty)
+                        .map((r) => Flexible(
+                              child: BottomNavBarButton(
+                                icon: r.icon,
+                                identifier: r.identifier,
+                                title: r.navbarTitle,
+                                active: r.identifier ==
+                                    routes.currentRoute.identifier,
+                              ),
+                            )),
+                    const Divider(
+                      direction: Axis.vertical,
+                      style: DividerThemeData(
+                        verticalMargin: EdgeInsets.symmetric(vertical: 10),
+                      ),
+                    ),
+                    Flexible(
+                      child: FlyoutTarget(
                         controller: routes.bottomNavFlyoutController,
                         child: IconButton(
                           icon: const Icon(FluentIcons.more),
-                          onPressed: () => routes.bottomNavFlyoutController.showFlyout(
+                          onPressed: () =>
+                              routes.bottomNavFlyoutController.showFlyout(
                             dismissWithEsc: true,
-                            builder: (context) => MenuFlyout(items: [
-                              for (var route in routes.allRoutes.where((r) => r.navbarTitle.isEmpty))
-                                MenuFlyoutItem(
-                                  leading: Icon(route.icon),
-                                  text: Txt(route.title),
-                                  onPressed: () => routes.navigate(routes.getByIdentifier(route.identifier)!),
-                                  closeAfterClick: true,
-                                )
-                            ]),
+                            builder: (context) => MenuFlyout(
+                              items: [
+                                for (var route in routes.allRoutes
+                                    .where((r) => r.navbarTitle.isEmpty))
+                                  MenuFlyoutItem(
+                                    leading: Icon(route.icon),
+                                    text: Txt(route.title),
+                                    onPressed: () => routes.navigate(routes
+                                        .getByIdentifier(route.identifier)!),
+                                    closeAfterClick: true,
+                                  )
+                              ],
+                            ),
                           ),
                         ),
-                      )
-                    ],
-                  );
-                }),
+                      ),
+                    )
+                  ],
+                );
+              },
+            ),
           ),
         ),
       ),
@@ -65,6 +87,7 @@ class BottomNavBarButton extends StatelessWidget {
   final IconData icon;
   final String identifier;
   final bool active;
+
   const BottomNavBarButton({
     super.key,
     required this.title,
@@ -75,32 +98,38 @@ class BottomNavBarButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
-            decoration: BoxDecoration(
-              color: active ? Colors.blue : null,
-              borderRadius: BorderRadius.circular(10),
+    return SizedBox(
+      height: 60,
+      child: IconButton(
+        icon: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+              decoration: BoxDecoration(
+                color: active ? Colors.blue : null,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Icon(
+                icon,
+                color: active ? Colors.white : null,
+                size: 18,
+              ),
             ),
-            child: Icon(
-              icon,
-              color: active ? Colors.white : null,
-              size: 18,
+            const SizedBox(height: 2),
+            Txt(
+              title,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                color: active ? Colors.blue : null,
+              ),
             ),
-          ),
-          Txt(
-            title,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-              color: active ? Colors.blue : null,
-            ),
-          ),
-        ],
+          ],
+        ),
+        onPressed: () => routes.navigate(routes.getByIdentifier(identifier)!),
       ),
-      onPressed: () => routes.navigate(routes.getByIdentifier(identifier)!),
     );
   }
 }
